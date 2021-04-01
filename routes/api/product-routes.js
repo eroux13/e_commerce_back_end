40688d4db24722055998518e88afabd2c76df595
EJ <ej.roux13@gmail.com>
2021-03-31 23:57:28 -0700

    testing get all products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,20 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+      include: [
+          {
+            model: "Category",
+            attributes: ["id", "category_name"]
+          },
+          {
+            model: "Tag",
+            attributes: ["id", "tag_name"]
+          }
+      ]
+  }).then((productData) => {
+      res.json(productData);
+  }).catch((err => res.json(err)));
 });
 
 // get one product
